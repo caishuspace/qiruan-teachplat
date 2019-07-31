6b7134d5b938014237dcb7cee1c542fc3c999afd
zerofancy <yqmailsend@163.com>
2019-08-01 01:21:38 +0800

    创建项目

---
+++ src/main/java/team/qiruan/QiruanApplication.java
@@ -0,0 +1,13 @@
+package team.qiruan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiruanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QiruanApplication.class, args);
+	}
+
+}
