7dfc3c72ab4fd137a04b068cd80420f2f8a39259
dream_s_wang <2948927433@qq.com>
2019-09-08 14:09:54 +0800

    修正userinfo.name为user.name

--- src/main/java/team/qiruan/job/dao/JobImpl.java
+++ src/main/java/team/qiruan/job/dao/JobImpl.java
@@ -22,7 +22,7 @@ public class JobImpl implements JobDao{
 
     @Override
     public Job_more search(int id) {
-        List<Job_more> list = jdbcTemplate.query("SELECT job.id,userinfo.avatar,userinfo.name,userinfo.sex, job.address,job.salary,job.time,job.phone,job.qq,job.mail,job.detail FROM job,userinfo WHERE job.uid=userinfo.uid and job.id = ?", new Object[]{id}, new BeanPropertyRowMapper<Job_more>(Job_more.class));
+        List<Job_more> list = jdbcTemplate.query("SELECT job.id,userinfo.avatar,user.name,userinfo.sex, job.address,job.salary,job.time,job.phone,job.qq,job.mail,job.detail FROM job,userinfo,user WHERE job.uid=userinfo.uid and userinfo.uid=user.id and job.id = ?", new Object[]{id}, new BeanPropertyRowMapper<Job_more>(Job_more.class));
         if(list!=null && list.size()>0){
             Job_more job_more = list.get(0);
             return job_more;
@@ -47,7 +47,7 @@ public class JobImpl implements JobDao{
 
     @Override
     public List<Job_less> getAllJobs_Less() {
-        List<Job_less> res= jdbcTemplate.query("SELECT job.id,userinfo.avatar,userinfo.name,userinfo.sex, job.address,job.salary,job.time  FROM job,userinfo WHERE userinfo.uid=job.uid", new Object[]{}, new BeanPropertyRowMapper<Job_less>(Job_less.class));
+        List<Job_less> res= jdbcTemplate.query("SELECT job.id,userinfo.avatar,user.name,userinfo.sex, job.address,job.salary,job.time  FROM job,userinfo,user WHERE userinfo.uid=job.uid and userinfo.uid=user.id", new Object[]{}, new BeanPropertyRowMapper<Job_less>(Job_less.class));
         if(res.isEmpty()){
             return null;
         }
